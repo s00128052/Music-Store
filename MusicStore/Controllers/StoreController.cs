520244fb8899e130baef0c13c536eaac778c1625
s00128052 <s00128052@mail.itsligo.ie>
2014-10-02 22:29:55 +0100

    Initial commit of Music Store on master branch

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+
+            return message;
+        }
+        //
+        // GET: /Store/Details
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+
+            return message;
+        }
+    }
+}
